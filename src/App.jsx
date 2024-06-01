4a502adc44055aaf52db06469d46bf607b53f815
Skeet Booth <skeetbooth@mac.com>
2024-06-01 18:31:42 +1000

    Add tailwind, set up base screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { ReactP5Wrapper } from '@p5-wrapper/react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const sketch = (p5) => {
+    p5.setup = () => p5.createCanvas(900, 900, p5.WEBGL)
+    p5.draw = () => {
+      p5.background(0)
+      p5.normalMaterial()
+      p5.push()
+      p5.rotateZ(p5.frameCount * 0.01)
+      p5.rotateX(p5.frameCount * 0.01)
+      p5.rotateY(p5.frameCount * 0.01)
+      p5.plane(500)
+      p5.pop()
+    }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className=''>
+      <h1 className='text-white font-bold p-5'>P5 Playground</h1>
+      <div className='rounded-lg bg-white'>
+        <ReactP5Wrapper sketch={sketch} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export const content = ['./src/**/**.{html,js,jsx}']
+export const theme = {
+  extend: {
+    colors: {
+      primary: '#4d6cef',
+      'primary-light': '#8887f9',
+    },
+  },
+}
+export const plugins = []
